8d30214a116adaa12807160189f754a02ed50af2
krdheeraj51 <krdheeraj51@gmail.com>
2022-08-21 17:51:53 +0530

    changes for arrow function and spread operator.

--- next-gen-typescipt/src/app.ts
+++ next-gen-typescipt/src/app.ts
@@ -1,1 +1,48 @@
 // Code goes here!
+
+const userName='Dheeraj';
+// userName="manish"
+let age=29;
+
+age=30;
+
+// function add(a:number,b:number){
+// let result;
+//     result= a+b;
+//     return result;
+
+// }
+
+
+// const add=(a:number,b:number)=>{
+//     let result;
+
+//     result= a+b;
+//     return result;
+// }
+// arrow function passing with default value
+// const add=(a:number,b:number=5) => a+b;
+
+// const printOut:(a:number| string)=>void=output=>console.log(output);
+// printOut(add(6));
+
+
+// spread operator
+
+const hobbies=["reading","cooking"];
+const activeHobbies=['Hiking'];
+activeHobbies.push(...hobbies)
+
+const person={
+    name:"Dheeraj",
+    age:28
+}
+
+let anotherObj={...person}
+
+console.log(anotherObj)
+
+//Rest Parameters...
+
+co
+
