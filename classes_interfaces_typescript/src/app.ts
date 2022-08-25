710f348a676ee7d4c3a214f430502295ecc5b264
krdheeraj51 <krdheeraj51@gmail.com>
2022-08-25 15:54:46 +0530

    learning more concept about type-script classes and interface.

--- classes_interfaces_typescript/src/app.ts
+++ classes_interfaces_typescript/src/app.ts
@@ -1,101 +1,40 @@
-// Code goes here!
-class Department {
-  //  private name: string;
-  protected employees: string[] = [];
+/**
+ * interface inheritance 
+ */
 
-  constructor(private id: string, public name: string) {
-    // this.name = n;
-  }
+interface Named{
+readonly name:string;
 
-  static createEmployee(name: string) {
-    return { name: name };
+}
+
+interface Greetable extends Named{
+  // name:string;
+  age:number;
+  greet(phrase: string):void
+}
+
+
+/**
+ * Classes implements interface 
+ */
+class Person implements Greetable{
+  name: string;
+  age=30;
+  constructor(n:string){
+    this.name=n
   }
-  describe(this: Department) {
-    console.log(`Department : ${this.id} -> ${this.name}`);
-  }
-  addEmployees(employee: string) {
-    this.employees.push(employee);
-  }
-  printEmployeeInformation() {
-    console.log(this.employees.length);
-    console.log(this.employees);
+  greet(phrase:string){
+    console.log(phrase +' '+this.name)
+    
   }
 }
 
-class ITDepartment extends Department {
-  constructor(id: string, public admins: string[]) {
-    super(id, "IT");
-  }
+let user1:Greetable;
+user1={
+name:'Max',
+age:15,
+greet(phrase:string){
+  console.log(phrase +''+this.name);
 }
-
-const accounting = new Department("d1", "Accounting");
-accounting.addEmployees("Mannu");
-accounting.addEmployees("Millan");
-// accounting.employees[2]='Manish';
-accounting.printEmployeeInformation();
-console.log(accounting);
-const accoutingCopy = { name: "Dheeraj", describe: accounting.describe };
-accounting.describe();
-
-// accoutingCopy.describe();
-
-// ITDepartment
-
-let itDepartment = new ITDepartment("id", ["max"]);
-
-itDepartment.addEmployees("Mannu");
-itDepartment.addEmployees("Millan");
-// accounting.employees[2]='Manish';
-itDepartment.printEmployeeInformation();
-
-console.log(itDepartment);
-
-class AccountingDepartment extends Department {
-  private lastReport: string;
-  get mostRecentReport() {
-    console.log("last Repot ", this.lastReport);
-    if (this.lastReport) {
-      return this.lastReport;
-    }
-    throw new Error("No Report found");
-  }
-
-  set mostRecentReport(value: string) {
-    if (!value) {
-      throw new Error("Please pass in a valid value");
-    }
-    this.addReports(value);
-  }
-
-  constructor(id: string, private reports: string[]) {
-    super(id, "Accounting");
-    console.log("reports ::", reports);
-    this.lastReport = reports[0];
-  }
-  addReports(text: string) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
-  printReports() {
-    console.log(this.reports);
-  }
-  addEmployees(name: string) {
-    if (name !== "Max") return;
-    this.employees.push(name);
-  }
 }
-
-const accountingDepart = new AccountingDepartment("d2", []);
-accountingDepart.mostRecentReport = "Here is report available";
-accountingDepart.addReports("Something went wrong");
-console.log(accountingDepart.mostRecentReport);
-
-// accountingDepart.printReports();
-
-// accountingDepart.addEmployees("divya");
-
-console.log(accountingDepart.mostRecentReport);
-
-const employee1= Department.createEmployee('Max');
-
-console.log(employee1)
+user1.greet('Hi there I am -')
